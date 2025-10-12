48871e493799d1ef714805e8a99be0bafa9ab024
DHANUSH KRISHNA CHEETIRALA <143580735+dhanush-730@users.noreply.github.com>
2025-10-12 14:57:17 +0530

    Delete backend directory

--- backend/models/YieldPrediction.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-
-class YieldPrediction(BaseModel):
-    temperature: float
-    rainfall: float
-    humidity: float
-    soil_type: str
-    weather_condition: str
-    crop_type: str
